c67b8a8713fab77342b666d480eb3eac8fb973c2
yumyumcurryman <micahkelias@gmail.com>
2023-01-03 18:51:23 -0800

    more finalized

--- lua/mke/core/keymaps.lua
+++ lua/mke/core/keymaps.lua
@@ -16,3 +16,18 @@ keymap.set("n", "<leader>t", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>c", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>p", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>q", ":tabp<CR>") --  go to previous tab
+
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
