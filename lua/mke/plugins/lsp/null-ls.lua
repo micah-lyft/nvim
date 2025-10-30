09032838f2191da864f12a877b3b1f66925d38c5
Micah Elias <micahelias@lyft.com>
2025-10-30 17:03:24 -0400

    updated work config to work with vim 11

--- lua/mke/plugins/lsp/null-ls.lua
+++ lua/mke/plugins/lsp/null-ls.lua
@@ -1,4 +1,4 @@
--- import null-ls plugin safely
+-- import none-ls plugin safely (maintained fork of null-ls)
 local setup, null_ls = pcall(require, "null-ls")
 if not setup then
   return
@@ -19,12 +19,13 @@ null_ls.setup({
     --  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
     formatting.prettier, -- js/ts formatter
     formatting.stylua, -- lua formatter
-    diagnostics.eslint_d.with({ -- js/ts linter
-      -- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-      condition = function(utils)
-        return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-      end,
-    }),
+    -- NOTE: eslint_d was removed from none-ls core
+    -- To use eslint_d, install none-ls-extras.nvim or use eslint language server
+    -- diagnostics.eslint_d.with({ -- js/ts linter
+    --   condition = function(utils)
+    --     return utils.root_has_file(".eslintrc.js")
+    --   end,
+    -- }),
   },
   -- configure format on save
   on_attach = function(current_client, bufnr)
